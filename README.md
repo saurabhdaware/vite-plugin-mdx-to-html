4f6d1a9d15b555519e1b53a0bd87898d4245403a
saurabhdaware <saurabhdaware99@gmail.com>
2022-09-03 19:49:56 +0530

    fix: imports in mdx

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # vite-plugin-mdx-to-html
-Vite Plugin to turn MDX into HTML string 
+
+Vite Plugin to turn MDX into HTML string.
+
+Stackblitz Example: 
+
+> **Note**
+>
+> Looking for using mdx in your React App or some framework? you might want to use [`@mdx-js/rollup`](https://mdxjs.com/packages/rollup/) as [suggested by MDX in their documentation](https://mdxjs.com/docs/getting-started/#bundler). This plugin can be used when you're using vanilla js setup or when you want to use mdx in a static-site-generator based on vite.
+
+
+## Installation
+
+```sh
+npm install --save-dev vite-plugin-mdx-to-html
+
+# OR
+
+yarn add --dev vite-plugin-mdx-to-html
+```
+
+Then in `vite.config.ts` or `vite.config.js` add the following code 
+
+## Usage
+
+```js
+import { defineConfig } from 'vite';
+import { vitePluginMdxToHTML } from 'vite-plugin-mdx-to-html';
+
+export default defineConfig({
+  plugins: [vitePluginMdxToHTML()],
+});
+```
+
+You can now import `.mdx` files anywhere in your vite app.
+
+
+```js
+import intro from './intro.mdx';
+
+document.querySelector('#root').innerHTML = intro;
+```
--- plugin/index.ts
+++ plugin/index.ts
@@ -1,21 +1,40 @@
+import path from 'path';
 import React from "react";
 import { renderToStaticMarkup, renderToString } from "react-dom/server";
 import runtime from 'react/jsx-runtime';
-import { evaluateSync } from '@mdx-js/mdx';
+import { evaluate } from '@mdx-js/mdx';
+import { ViteDevServer } from 'vite';
 
 type PluginOptions = {
   renderFunc?: 'renderToStaticMarkup' | 'renderToString';
 }
 
 export const vitePluginMdxToHTML = (pluginOptions?: PluginOptions) => {
+  let server: ViteDevServer;
   return {
     name: "vite-plugin-mdx-to-html",
-    transform(source: string, id: string) {
+    configureServer(_server: ViteDevServer) {
+      server = _server;
+    },
+    async transform(source: string, id: string) {
+      const baseUrl = `file://${path.dirname(id)}/`;
       if (id.endsWith(".mdx")) {
-        const mdx = evaluateSync(source, {
+        console.log({source});
+        const mdx = (await evaluate(source, {
           ...runtime as any,
-        }).default;
+          useDynamicImport: true,
+          baseUrl
+        })).default;
 
+        // const importsInMdx = Array.from(source.matchAll(/ from ["'](.*?)["']/g))
+        //   .map((mdxImportMatch) => `import '${mdxImportMatch[1]}';\n`);
+
+        // console.log(String(mdx));
+        // // server.watcher.addListener('change', (filename) => {
+        // //   if (filename.includes('math.js')) {
+        // //     server.watcher.emit('change', id);
+        // //   }
+        // // });
         let html;
         if (pluginOptions?.renderFunc === 'renderToString') {
           html = renderToString(React.createElement(mdx));
